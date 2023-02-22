48232046ce9bd0d74ac8dfa0e6065c52ac6a22c8
henrique almeida <henrique.almeida231@hotmail.com>
2023-02-22 09:37:22 -0300

    resposive fix errors and add image for resposive plataforms

--- src/components/paterns/Paterns.tsx
+++ src/components/paterns/Paterns.tsx
@@ -4,10 +4,10 @@ export const Paterns = () => {
     return (
         <section className={styles.paterns}>
             <ul className={styles.ulpaterns}>
-                <li><img src='/coinbase.png'></img></li>
-                <li><img src='/blockchain.png'></img></li>
-                <li><img src='/binance.png'></img></li>
-                <li><img src='/bitmex.png'></img></li>
+                <li><img src='/coinbase.png' className={styles.ma850}></img><img src='/coinbase-.png' className={styles.me850}></img></li>
+                <li><img src='/blockchain.png' className={styles.ma850}></img><img src='/blockchain-.png' className={styles.me850}></img></li>
+                <li><img src='/binance.png' className={styles.ma850}></img><img src='/binance-.png' className={styles.me850}></img></li>
+                <li><img src='/bitmex.png' className={styles.ma850}></img><img src='/bitmex-.png' className={styles.me850}></img></li>
             </ul>
         </section>
     )
